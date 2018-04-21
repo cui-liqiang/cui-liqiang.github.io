be95a29282140e44e3752cc03597a1490527af64
cui-liqiang <cui.liqiang@gmail.com>
2018-04-21 11:56:35 +0800

    added gio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var _vds = _vds || [];
+window._vds = _vds;
 (function($){
   // Search
   var $searchWrap = $('#search-form-wrap'),
@@ -134,4 +136,13 @@
 
     $container.removeClass('mobile-nav-on');
   });
+  _vds.push(['setAccountId', 'aee85976ffc99859']);
+        (function() {
+          var vds = document.createElement('script');
+          vds.type='text/javascript';
+          vds.async = true;
+          vds.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'assets.growingio.com/vds.js';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(vds, s);
+        })();
 })(jQuery);
